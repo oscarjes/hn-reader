1b668b195276d76243299f1bae4c0138726cd294
oscarjes <oscar@jesionek.co>
2018-04-30 11:57:43 +0700

    comments for offline support

--- src/App.js
+++ src/App.js
@@ -22,20 +22,32 @@ class App extends Component {
 
   async fetchStory(storyId) {
     const stories = this.state.stories;
-    // Retrieve story details
+
+    // Fetch story details
     const results = await fetch(
       `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
     );
     const story = await results.json();
-    // Add story to stories array in state
-    let storiesInLocalStorage = localStorage.getItem('stories');
+
+    // Retrieve stories already stored in localStorage
+    let storiesInLocalStorage = localStorage.getItem("stories");
+
+    // Check if it's the first story, needed for proper JSON format
     if (storiesInLocalStorage) {
-      localStorage.setItem('stories', storiesInLocalStorage + ',' + JSON.stringify(story));
-      localStorage.setItem('storiesArray', '[' + storiesInLocalStorage + ']');
+      // If it's not the first story then add a comma before adding object
+      localStorage.setItem(
+        "stories",
+        storiesInLocalStorage + "," + JSON.stringify(story)
+      );
+      // It needs to be an array of objects, so add brackets before and after
+      localStorage.setItem("storiesArray", "[" + storiesInLocalStorage + "]");
     } else {
-      localStorage.setItem('stories', JSON.stringify(story));
+      localStorage.setItem("stories", JSON.stringify(story));
     }
+
+    // Add story to stories array in state
     stories.push(story);
+    
     // Force render since we want each story to be displayed immediately
     // otherwise react will only update after fetching all of the items
     this.setState(this.state);
@@ -57,27 +69,37 @@ class App extends Component {
     // Activate spinner
     this.setState({ isLoading: true });
 
+    // Offline support: if not online then set state from localStorage
     if (!navigator.onLine) {
-      this.setState({ newStoryIds: JSON.parse(localStorage.getItem('newStoryIds')) });
-      this.setState({ stories: JSON.parse(localStorage.getItem('storiesArray')) });
+      this.setState({
+        newStoryIds: JSON.parse(localStorage.getItem("newStoryIds"))
+      });
+      this.setState({
+        stories: JSON.parse(localStorage.getItem("storiesArray"))
+      });
     }
-    
+
     // Retrieve the ids of the 500 latest stories
     const results = await fetch(
       "https://hacker-news.firebaseio.com/v0/newstories.json"
     );
     const newStoryIds = await results.json();
-    // Save the story ids to state
+
+    // Save the story ids to state & to localStorage for offline support
     this.setState({ newStoryIds: newStoryIds });
-    localStorage.setItem('newStoryIds', JSON.stringify(newStoryIds));
+    localStorage.setItem("newStoryIds", JSON.stringify(newStoryIds));
+
     // Grab the ids of the next 20 stories
     const twentyStories = this.nextTwentyStories(this.state.newStoryIds);
+
     // Fetch the details for the 20 stories
     for (let story of twentyStories) {
       await this.fetchStory(story);
     }
+
     // Deactivate spinner
     this.setState({ isLoading: false });
+
     // Add event listener for scrolling +
     // throttle onScroll using lodash to improve performance
     window.addEventListener("scroll", _.throttle(this.onScroll, 16), false);
